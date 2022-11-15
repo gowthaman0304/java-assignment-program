71064bfba0bc888da8f6ba3c9055f7630a6a732e
gowthaman0304 <gowthamg099@gmail.com>
2022-11-15 18:34:31 +0530

    Array program

---
+++ Array.java
@@ -0,0 +1,16 @@
+class Array
+{
+public static void main(String[] args)
+{
+int marks[] ={83,63,88,89,94};
+for(int i=0;i<marks.length;i++)
+{
+System.out.println(marks[i]);
+}
+//System.out.println("Tamil"+" "+marks[0]);
+//System.out.println("English"+" "+marks[1]);
+//System.out.println("Maths"+" "+marks[2]);
+//System.out.println("Scince"+" "+marks[3]);
+//System.out.println("Social"+" "+marks[4]);
+}
+}
